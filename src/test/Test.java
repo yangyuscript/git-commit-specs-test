dc83207714bc7b9d5ae8de8a50bd226ae326498e
yangyuscript <1837475870@qq.com>
2019-04-20 20:30:07 +0800

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @Description:
+ * @Author: lingx
+ * @Date: Created in 2019/4/20 20:28
+ * @Modified By:
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
