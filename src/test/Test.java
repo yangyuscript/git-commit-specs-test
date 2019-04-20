3f32b9b2bb8b72095d9000fb0148aea739ba9594
yangyuscript <1837475870@qq.com>
2019-04-20 20:53:13 +0800

    :loud_sound: 测试

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,5 +9,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("this is emoji commit test");
     }
 }
