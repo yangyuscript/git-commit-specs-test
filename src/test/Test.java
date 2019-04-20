ab9743ec2657bdc5dbc84c9d763c5cbf59841d8c
yangyuscript <1837475870@qq.com>
2019-04-20 21:35:01 +0800

    test commit message

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is emoji commit test");
+        System.out.println("test git commit message");
     }
 }
